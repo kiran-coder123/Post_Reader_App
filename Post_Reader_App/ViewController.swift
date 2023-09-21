d6a15f62c32b9a42ffa61e8c10fe5a6768711519
Kiran Sonne <kiran.sonne@neosoftmail.com>
2023-09-21 11:34:57 +0530

    Initial Commit

---
+++ Post_Reader_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Post_Reader_App
+//
+//  Created by Neosoft on 21/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Post_Reader_AppUITests/Post_Reader_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Post_Reader_AppUITestsLaunchTests.swift
+//  Post_Reader_AppUITests
+//
+//  Created by Neosoft on 21/09/23.
+//
+
+import XCTest
+
+final class Post_Reader_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
